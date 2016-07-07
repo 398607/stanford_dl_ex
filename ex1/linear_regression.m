7984a23ef32d19c2d095195ecf2f98e091f9af52
398607 <nagizero@foxmail.com>
2016-07-07 17:07:20 +0800

    first

--- ex1/linear_regression.m
+++ ex1/linear_regression.m
@@ -13,13 +13,11 @@ function [f,g] = linear_regression(theta, X, y)
   f=0;
   g=zeros(size(theta));
   
-  for i = 1 : m
-      f = f + (theta'*X(:, i) - y(i))^2 / 2;
-  end
+  f = sum((theta'*X - y).^2);
   
   for j = 1 : n
       for i = 1 : m
-          
+          g(j) = g(j) + X(j, i) * (theta'*X(:, i) - y(i));
       end
   end
   %
